e4530ff8a0090ad15972d2b97703fad9717a9edb
Vishaal Prasad <vprasad@guru>
2015-01-22 11:00:20 -0800

    initial commit

---
+++ custom_autoencoder.py
@@ -0,0 +1,98 @@
+import functools
+
+import numpy as np
+
+from pylearn2.models.autoencoder import DenoisingAutoencoder
+from pylearn2.models.model import Model
+from pylearn2.linear.matrixmul import MatrixMul
+
+
+
+class CustomDenoisingAutoencoder(DenoisingAutoencoder):
+    """
+    A denoising autoencoder 
+
+    Parameters
+    ----------
+    See: DenoisingAutoencoder's parameters
+    nhid = number of hidden units
+    numCons = number of connections per hidden unit
+    sigma = standard deviation matrix
+    imageSize = size of an image
+        """
+    def __init__(self, nhid, numCons, sigma, imageSize, corruptor,
+            act_enc, act_dec, nvis, tied_weights=False, irange=1e-3, rng=9001):
+
+        super(CustomDenoisingAutoencoder, self).__init__(
+                corruptor,
+                nvis, 
+                nhid,
+                act_enc,
+                act_dec,
+                tied_weights,
+                irange,
+                rng
+        )
+        hiddenUnitLocs = self._createHiddenUnits(nhid, imageSize)
+        matrix = self._createConnectionMatrix(imageSize, hiddenUnitLocs, numCons, sigma)
+        self.mask = matrix.transpose()
+
+
+    def _createHiddenUnits(self, numHiddenUnits, imageSize):
+        return np.array([[16, 16]])
+
+    def _createConnectionMatrix(self, imageSize, hiddenUnitLocs, numConnections, sigma):
+
+        ## Define some useful local variables for sake of clarity ##
+        imgHeight = imageSize[0]
+        imgLength = imageSize[1]
+        numPixels = imgHeight * imgLength
+        numHiddenUnits = len(hiddenUnitLocs)
+
+        ## Initialize the Connection Matrix to all zeroes ##
+        connectionMatrix = np.zeros(shape=(numHiddenUnits,numPixels))
+        currHiddenUnit = 0 # index to keep track of which hidden unit we're working on.
+
+        ## Create a variance parameter by squaring each element in sigma, used in Gaussian ##
+        variance = [[elem * elem for elem in inner] for inner in sigma]
+
+        ## Loop through the Hidden Units to Create Samples ##
+        for k in hiddenUnitLocs:
+
+            i = 0
+            while i < numConnections:
+
+                # Get random Gaussian sample which returns an array of tuples
+                [[x, y]] = np.random.multivariate_normal(k, variance, 1)
+
+                # Round the sample to nearest integer
+                x = round(x, 0)
+                y = round(y, 0)
+
+                # Check to see if it's out of bounds.
+                if (x >= imgLength) or (y >= imgHeight) or (x < 0) or (y < 0):
+                    continue
+
+                # Calculate which pixel number it is to add to the map.
+                pixelLoc = (y) * imgLength + (x)
+
+                if (connectionMatrix[currHiddenUnit][pixelLoc] == 1):
+                    continue
+            
+                connectionMatrix[currHiddenUnit][pixelLoc] = 1
+                i += 1
+
+            currHiddenUnit += 1
+
+        return connectionMatrix
+    
+
+
+    @functools.wraps(Model._modify_updates)
+    def _modify_updates(self, updates):
+        W = self.weights
+        if W in updates:
+            updates[W] = updates[W] * self.mask
+
+
+
---
+++ vanhateren.py
@@ -0,0 +1,70 @@
+from theano.compat.six.moves import xrange
+
+from pylearn2.datasets import cache, dense_design_matrix
+from pylearn2.expr.preprocessing import global_contrast_normalize
+from pylearn2.utils import contains_nan
+from pylearn2.utils import serial
+from pylearn2.utils import string_utils
+
+import glob
+import numpy as np
+import array
+
+
+class VANHATEREN(dense_design_matrix.DenseDesignMatrix):
+
+    def __init__(self, which_set, axes=('b', 0, 1, 'c')):
+        # note: there is no such thing as the cifar10 validation set;
+        # pylearn1 defined one but really it should be user-configurable
+        #(as it is here)
+        self.axes = axes
+        #we define here:
+        dtype = 'uint16'
+        ntrain = 200
+        nvalid = 0  # artefact, we won't use it
+        ntest = 50
+        self.img_shape = (32, 32)
+        self.img_size = np.prod(self.img_shape)
+        #Get files
+        files = '/home/vprasad/data/vanhateren/*.iml'
+        images = glob.glob(files)
+
+        trainX = np.empty(shape=(ntrain, self.img_size))
+        testX = np.empty(shape=(ntest, self.img_size))
+
+        i = 0
+        #take 250 images, convert to 32x32, store in X
+        for image in images:
+            with open(image, 'rb') as handle:
+                s = handle.read()
+            arr = array.array('H', s)
+            arr.byteswap()
+
+            width = 1536
+            height = 1024
+
+            img = np.array(arr, dtype='uint16').reshape(height, width)
+            left_margin = (width-32)/2
+            top_margin = (height-32)/2
+            if (i < ntrain):
+                trainX[i] = img[top_margin: top_margin+32, left_margin:left_margin+32].flatten()
+                
+            else:
+                testX[i-ntrain] = img[top_margin: top_margin+32, left_margin:left_margin+32].flatten()
+            i = i + 1
+        testMax = testX.max()
+        trainMax = trainX.max()
+        trainX = trainX / 1.
+        trainX = trainX / max(testMax, trainMax)
+        testX = testX / 1.
+        testX = testX / max(testMax, trainMax)
+        #call super on X
+        
+        view_converter = dense_design_matrix.DefaultViewConverter((32, 32, 1), axes)
+        if which_set == 'train':
+            super(VANHATEREN, self).__init__(X=trainX,view_converter = view_converter, axes = axes)
+        else: 
+            super(VANHATEREN, self).__init__(X=testX, view_converter = view_converter, axes = axes)
+
+
+
