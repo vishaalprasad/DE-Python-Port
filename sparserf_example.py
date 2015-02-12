4c1ae0a82a7fdb8c4d0e6e00ecb614598a419d48
Ben Cipollini <bcipolli@ucsd.edu>
2015-02-12 14:35:44 -0800

    Templatize sparserf_template.yaml

--- sparserf_example.py
+++ sparserf_example.py
@@ -1,18 +1,30 @@
 
 if __name__ == "__main__":
+    import tempfile
+
+    weights_file = 'sparserf_example.pkl'
+    params = (10, [[3, 0], [0, 3]], weights_file)
 
     # Create the dataset
     from de.datasets import VanHateren
     VanHateren.create_datasets()
 
-    # Train the network.
+    # Create the yaml file.
+    _, config_fn = tempfile.mkstemp()
+    with open("sparserf_template.yaml") as fp:
+        # create yaml from templtes + params
+        config_yaml = "".join(fp.readlines()) % params
+    with open(config_fn, 'w') as config_fp:
+        config_fp.write(config_yaml)
+
+    # Train the network
     from pylearn2.scripts.train import train
-    train(config="sparserf.yaml")
+    train(config=config_fn)
 
     # Visualize the weights
     from pylearn2.scripts.show_weights import show_weights
-    show_weights(model_path="sparserf.pkl", border=True)
+    show_weights(model_path=weights_file, border=True)
 
     # Visualize the reconstruction
     from de.compare_reconstruct import compare_reconstruction
-    compare_reconstruction(model_path="sparserf.pkl")
+    compare_reconstruction(model_path=weights_file)
