eb1da9ed4d4347fcb65263233647ef816179d6e8
Vishaal <v2prasad@ucsd.edu>
2015-02-17 11:25:50 -0800

    Moved hemispherical_differences into fft_analyze. Gave credit for radial*.py

--- fft_analyze.py
+++ fft_analyze.py
@@ -23,7 +23,9 @@ def fft2AverageOnImageSet(images):
     return total_frequency / float(len(images))
 
 
-if __name__ == '__main__':
+# A function that performs an fft analysis of an image and its reconstruction
+# and plots the analyses for purposes of visualization.
+def singleImageAnalysis(model_path):
 
     print("Loading the training set...")
     train_img = os.path.join(DATA_DIR, 'train.pkl')
@@ -32,7 +34,6 @@ if __name__ == '__main__':
 
     # Run the model and visualize
     print("Loading the model...")
-    model_path = 'savedata.pkl'
     model = serial.load(model_path)
 
     list_of_images = []
@@ -84,3 +85,98 @@ if __name__ == '__main__':
 
     plt.savefig('fft1D.png')
     os.system('eog fft1D.png &')
+
+
+# A function that helps visualize the differences between each
+# hemispherical representation of a set of images.
+def hemisphericalDifferences(left_model_path, right_model_path):
+    print("Loading the training set...")
+    train_img = os.path.join(DATA_DIR, 'train.pkl')
+    train_set = serial.load(train_img)
+    patch_size = (32, 32)
+
+    # Load both models
+    print("Loading the models...")
+    left_model = serial.load(left_model_path)
+    right_model = serial.load(right_model_path)
+
+    # Create empty arrays to hold images
+    list_of_images = []
+    list_of_left_reconstructed = []
+    list_of_right_reconstructed = []
+
+    print("Beginning the fft analysis...")
+
+    # Go through all the images and add them to the lists
+    for ii in np.arange(train_set.X.shape[0]):
+        img_vector = train_set.denormalize_image(train_set.X[ii, :])
+        img_patch = img_vector.reshape(patch_size)
+        list_of_images.append(img_patch)
+
+        # Part for reconstructed
+        [tensor_var] = left_model.reconstruct([img_vector])
+
+        reconstructed_vector = tensor_var.eval()
+        reconstructed_patch = train_set.denormalize_image(reconstructed_vector)
+        reconstructed_patch = reconstructed_patch.reshape(patch_size)
+
+        list_of_left_reconstructed.append(reconstructed_patch)
+
+        [tensor_var] = right_model.reconstruct([img_vector])
+
+        reconstructed_vector = tensor_var.eval()
+        reconstructed_patch = train_set.denormalize_image(reconstructed_vector)
+        reconstructed_patch = reconstructed_patch.reshape(patch_size)
+
+        list_of_right_reconstructed.append(reconstructed_patch)
+
+    # Run 2D Analysis
+    average_frequency = fft2AverageOnImageSet(list_of_images)
+    average_left_reconstructed = fft2AverageOnImageSet(
+        list_of_left_reconstructed
+        )
+    average_right_reconstructed = fft2AverageOnImageSet(
+        list_of_right_reconstructed
+        )
+
+    # Run 1D Analysis
+    psd1D = radialProfile.azimuthalAverage(average_frequency)
+    reconstructed_left_psd1D = radialProfile.azimuthalAverage(
+        average_left_reconstructed
+        )
+    reconstructed_right_psd1D = radialProfile.azimuthalAverage(
+        average_right_reconstructed
+        )
+
+    # Plot 3 1D images: The Original, the reconstructions from LH and RH
+    fg = plt.figure()
+    fg.add_subplot(1, 3, 1)
+    plt.plot(np.log(psd1D))
+    plt.title('Original')
+
+    fg.add_subplot(1, 3, 2)
+    plt.plot(np.log(reconstructed_left_psd1D))
+    plt.title('LH Reconstructed')
+
+    fg.add_subplot(1, 3, 3)
+    plt.plot(np.log(reconstructed_right_psd1D))
+    plt.title('RH Reconstructed')
+
+    plt.savefig('power.png')
+    os.system('eog power.png &')
+
+    # Get difference of differences
+    left_difference = abs(reconstructed_left_psd1D - psd1D)
+    right_difference = abs(reconstructed_left_psd1D - psd1D)
+    total_difference = abs(left_difference-right_difference)
+    plt.figure()
+    plt.plot(total_difference)
+    plt.title('Difference in power differences')
+
+    plt.savefig('differences.png')
+    os.system('eog differences.png &')
+
+
+if __name__ == '__main__':
+    # singleImageAnalysis('savedata.pkl')
+    hemisphericalDifferences('left.pkl', 'right.pkl')
