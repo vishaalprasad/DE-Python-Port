7f044696f1dc6db34346c21fca0d2293100687c5
Vishaal <v2prasad@ucsd.edu>
2015-03-12 12:15:51 -0700

    More generic experiment better suited mult models

--- dae_mlp.py
+++ dae_mlp.py
@@ -1,14 +1,20 @@
-import sys; sys.path = ['/raid/vprasad/pylearn2'] + sys.path
-
-def create_classifier():
+def create_classifier(pkl_file, save_path):
     # Create the dataset
     from de.datasets import Sergent
     from de import sparserf_autoencoder
     Sergent.create_datasets(overwrite=True)
 
     # Train the network.
-    from pylearn2.scripts.train import train
-    train(config="dae_mlp.yaml")
+    #from pylearn2.scripts.train import train
+
+    layer1_yaml = open('dae_mlp.yaml', 'r').read()
+    hyper_params = {'pkl_file' : pkl_file,
+                    'save_path' : save_path}
+    layer1_yaml = layer1_yaml % hyper_params
+    from pylearn2.config import yaml_parse
+    train = yaml_parse.load(layer1_yaml)
+    train.main_loop()
+    #train(config=layer1_yaml)
 
 if __name__ == "__main__":
-    create_classifier()
+    create_classifier("sparserf_example.pkl", "final.pkl")
--- de_experiment.py
+++ de_experiment.py
@@ -4,5 +4,8 @@ from sparserf_example import create_sparserf
 from dae_mlp import create_classifier
 
 if __name__ == "__main__":
-    create_sparserf(10, [[3, 0], [0, 3]], 'sparserf_example.pkl')
-    create_classifier()
+    create_sparserf(10, [[4, 0], [0, 4]], 'left_hemisphere.pkl')
+    create_classifier('left_hemisphere.pkl', 'left_final.pkl')
+
+    create_sparserf(10, [[3, 0], [0, 3]], 'right_hemisphere.pkl')
+    create_classifier('right_hemisphere.pkl', 'right_final.pkl')
